bc0e0f2cdd9e804b8bdfd0404916fee873e836ab
Vladislav Brezgun <nextydev@gmail.com>
2021-09-21 19:30:50 +0300

    added routes and controller for auth

---
+++ controllers/auth.js
@@ -0,0 +1,7 @@
+module.exports.login = (res, req) => {
+    // controller logic
+}
+
+module.exports.register = (res, req) => {
+    // controller logic
+}
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const controller = require('../controllers/auth');
+const router = express.Router();
+
+// api/auth/login
+router.post('/login', controller.login);
+
+// api/auth/register
+router.post('/register', controller.register);
